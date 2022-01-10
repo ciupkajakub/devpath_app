cfa7dea0ff219872cd64ff7f30bb763544375458
ciupkajakub <ciupkajakub@gmail.com>
2022-01-10 11:25:55 +0100

    Added possibility to search purchases using aasm_state

--- app/controllers/admins/purchases_controller.rb
+++ app/controllers/admins/purchases_controller.rb
@@ -2,7 +2,7 @@ class Admins::PurchasesController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    all_purchases = Purchase.where(aasm_state: 'bought')
+    all_purchases = Purchase.all
     @q = all_purchases.ransack(params[:q])
     @q.sorts = 'name asc'
     @purchases = @q.result(distinct: true).includes(:purchase_products, :user).
